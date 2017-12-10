8f322d6d525be3083171d0e067b565bafae69316
James Y <jamesy1@andrew.cmu.edu>
2017-12-10 01:00:23 -0500

    initial commit (wrote message)

---
+++ q20.R
@@ -0,0 +1,5 @@
+cat("Sometimes I feel like being human is kind of boring. Do you feel like being an alien is fun?
+    What color are you? Most people think when we find you you'll be green and slimy and have big slanty eyes,
+    but I don't know. I think you might be grey and yellow and have hands for eyes, or you might just like
+    just like humans but a little taller. My name is James by the way. What's your name? If aliens have names.")
+
